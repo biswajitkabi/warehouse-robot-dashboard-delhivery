00bf3bed2c13e983c1108dec42169ccc3d8586c4
Biswajit Kabi <biswajitkabi711@gmail.com>
2025-12-06 11:41:49 +0530

    feat: add useStore

---
+++ src/stores/useStore.js
@@ -0,0 +1,26 @@
+import create from "zustand";
+import { nanoid } from "nanoid";
+
+export const useStore = create((set, get) => ({
+  auth: { user: null, loggedIn: false },
+  bots: [], // will fill with mock bots
+  tasks: [],
+
+  // auth
+  login: (user) => set({ auth: { user, loggedIn: true } }),
+  logout: () => set({ auth: { user: null, loggedIn: false } }),
+
+  // bots
+  setBots: (bots) => set({ bots }),
+  updateBot: (id, patch) => set((state) => ({
+    bots: state.bots.map(b => b.id === id ? { ...b, ...patch } : b)
+  })),
+
+  // tasks
+  addTask: (task) => {
+    const t = { id: nanoid(), createdAt: Date.now(), ...task };
+    set((s) => ({ tasks: [...s.tasks, t] }));
+    return t;
+  },
+  removeTaskById: (id) => set((s) => ({ tasks: s.tasks.filter(t => t.id !== id) })),
+}));
